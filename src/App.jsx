e4e141e93afc67caccab83450a549d2302927400
samandargofurov <samandargofurov236@gmail.com>
2024-03-23 17:48:16 +0500

    7 lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,91 @@
-import { useState } from 'react'
+import { PacmanLoader } from 'react-spinners'
+import { useState, useEffect, useRef } from 'react'
+import Card from './components/Card'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState(0);
-  const [desc, setDesc] = useState('');
+  const [loader, setLoader] = useState(false);
+  const [phones, setPhones] = useState([]);
 
-  function handleClick() {
+  const nameRef = useRef('');
+  const priceRef = useRef(0);
+  const descRef = useRef('');
+  const statusRef = useRef('active');
 
+  useEffect(() => {
+    setLoader(true);
+    fetch("https://auth-rg69.onrender.com/api/products/all")
+      .then(res => res.json())
+      .then(data => {
+        setPhones(data)
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoader(false);
+      })
+  }, [])
+
+  function validate(name, price, desc) {
+
+    if (name.value.trim().length < 3) {
+      alert("Name is empty");
+      return false;
+    }
+  
+    if (desc.value.trim().length < 3) {
+      alert("Description is empty");
+      return false;
+    }
+  
+    if (price.value < 0) {
+      alert("price is not a number");
+      return false;
+    }
+
+    return true;
+  }
+
+  function handleClick(e) {
+    e.preventDefault();
+    const isValid = validate(name, price, desc);
+
+    if (isValid) {
+      const phone = {
+        name: nameRef.current.value,
+        desc: descRef.current.value,
+        price: priceRef.current.value,
+        status: statusRef.current.value,
+        category_id: 2
+      }
+
+      fetch("https://auth-rg69.onrender.com/api/products",{
+        method: 'POST',
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(phone)
+      })
+
+        .then(res => res.json())
+        .then(data => {
+          if (data.id) {
+            let copied = JSON.parse(JSON.stringify(phones))
+            copied.push(data)
+            setPhones(copied)
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .finally(() => {
+          nameRef.current.value = ''
+          priceRef.current.value = ''
+          statusRef.current.value = ''
+          descRef.current.value = ''
+        })
+    }
   }
 
   return (
@@ -15,18 +93,21 @@ function App() {
       <div className="container">
 
 {/* form */}
+
+        <h1 className='text-center'>Phone informations</h1>
+
         <form className='d-flex w-50 gap-3 flex-wrap flex-column mt-3 mx-auto'>
 
-          <input type="text" className="form-control" placeholder="Enter name" />
+          <input ref={nameRef} type="text" className="form-control" placeholder="Enter name" />
 
-          <input type="number" className="form-control" placeholder="Enter price" />
+          <input ref={priceRef} type="number" className="form-control" placeholder="Enter price" />
 
-          <select class="form-select" aria-label="Default select example">
-            <option selected>active</option>
+          <select ref={statusRef} className="form-select">
+            <option value="active">active</option>
             <option value="inactive">inactive</option>
           </select>
 
-          <textarea className="form-control" rows="3" placeholder="Enter description"></textarea>
+          <textarea ref={descRef} className="form-control" rows="3" placeholder="Enter description"></textarea>
 
           <button onClick={handleClick} className='btn btn-primary'>Submit</button>
 
@@ -34,18 +115,20 @@ function App() {
 
 {/* cards */}
 
-        <div className="cards d-flex flex-wrap gap-2 mt-5">
-
-          <div className="card w-25">
-            <div className="card-body">
-              <h5 className="card-name">Iphone 15 pro</h5>
-              <h6 className="card-status my-2 fs-4 text-body-secondary">active</h6>
-              <h6 className="card-price my-2 fs-5">$999</h6>
-              <p className="card-desc">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
+        <div className="cards d-flex flex-wrap gap-4 mt-5 justify-content-center">
+            {
+              loader && <PacmanLoader color="#36d7b7" size="2rem" />
+            }
+            {
+              !loader && phones.map((arg, index) => {
+                return (
+                  <Card key={index} phone={arg}></Card>
+                )
+              })
+            }
 
         </div>
+
       </div>
     </>
   )
---
+++ src/components/Card/index.jsx
@@ -0,0 +1,16 @@
+function Card(props) {
+
+    const {name, description, price, status} = props.phone
+    return (
+        <div className="card" style={{width: "18rem"}}>
+            <div className="card-body">
+              <h5 className="card-title">{name}</h5>
+              <h6 className="card-status my-2 fs-4 text-body-secondary">{status}</h6>
+              <h6 className="card-number my-2 fs-5">${price}</h6>
+              <p className="card-text">{description}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
